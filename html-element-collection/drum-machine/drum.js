2ca18fc6d0a0fb495cdb282c1928d9756ddf9000
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-04 19:26:30 +0300

    fixed task 2 drum set

---
+++ html-element-collection/drum-machine/drum.js
@@ -0,0 +1,10 @@
+'use strict';
+let drums = document.getElementsByClassName('drum-kit__drum');
+for (let drum of drums) {
+    drum.onclick = function () {
+        let player = this.getElementsByTagName('audio')[0];
+        player.currentTime = 0;
+        player.play();
+    };
+}
+;
