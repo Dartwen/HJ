681048251e30dd7f99a1ff204aea2d5b8f832ea4
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-06 03:08:20 +0300

    made task 1 weather widget

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,17 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+request.addEventListener("load", onLoad);
+request.addEventListener("error", onError);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+function onLoad() {
+    if (request.status === 200) {
+        const response = JSON.parse(request.responseText);
+        setData(response);
+    }else{
+        console.log(`Ответ ${xhr.status}: ${xhr.statusText}`);
+    }
 }
+function onError() {
+    console.log("Сработало событие error");
+}
+
