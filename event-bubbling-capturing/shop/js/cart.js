a74192941d4f069f3f94f8c5eb7599df63508dd1
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-13 04:07:42 +0300

    done task 3 shop

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function addBasket(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('add-to-cart')) {
+        addToCart({ title: event.target.dataset.title, price: event.target.dataset.price });
+    }
+}
+
+const addItems = document.querySelector('main.items-list');
+addItems.addEventListener('click', addBasket);
