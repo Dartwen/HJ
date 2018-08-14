2ecf7470ab73df5eae5e4637afa1783d54408e46
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-14 16:35:00 +0300

    done task 1 counter

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,32 @@
 'use strict';
+
+let increment = document.querySelector('#increment');
+let decrement = document.querySelector('#decrement');
+let reset = document.querySelector('#reset');
+let counter = document.querySelector('#counter');
+
+function updateConter() {
+    if (!localStorage.counter) {
+        localStorage.counter = 0;
+    }
+    counter.textContent = localStorage.counter;
+
+}
+
+increment.addEventListener('click', () => {
+    ++localStorage.counter;
+    updateConter();
+});
+
+decrement.addEventListener('click', () => {
+    if(localStorage.counter > 0) {
+        --localStorage.counter;
+    }
+    updateConter();
+});
+reset.addEventListener('click', () => {
+    localStorage.counter = 0;
+    updateConter();
+})
+
+document.addEventListener('DOMContentLoaded', updateConter);
