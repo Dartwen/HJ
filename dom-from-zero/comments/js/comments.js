f937c30bf03ae63cf93cbc63df020f3624846f13
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-29 18:11:05 +0300

    done task 1 comment

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -1,13 +1,49 @@
 'use strict';
 
 function showComments(list) {
-  const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+    const commentsContainer = document.querySelector('.comments');
+    const comments = list.map(createComment);
+    const fragment = comments.reduce((fragment, currentValue) => {
+        fragment.appendChild(currentValue);
+        return fragment;
+    }, document.createDocumentFragment());
+    commentsContainer.appendChild(fragment);
+}
+
+function el(tagName, attributes, children) {
+    const element = document.createElement(tagName);
+    if (typeof attributes === 'object') {
+        Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
+    }
+    if (typeof children === 'string') {
+        element.textContent = children;
+    } else if (children instanceof Array) {
+        children.forEach(child => element.appendChild(child));
+    }
+    return element;
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
+    const photoBlock = el('div', {class: 'photo', title: comment.author.name}, [el('div',{
+        class: 'avatar',
+        style: `background-image: url('${comment.author.pic}')`
+    })]);
+    const commentBlock = el('div', {class: 'comment-block'}, [
+        el('p', {class: 'comment-text'}, comment.text.split('\n').map(line => line ? el('p', {}, line) : el('br', {}, ''))),
+        el('div', {class: 'bottom-comment'}, [
+            el('div', {class: 'comment-date'}, `${new Date(comment.date).toLocaleString('ru-Ru')}`),
+            el('ul', {class: 'comment-actions'}, [
+                el('li', {class: 'complain'}, 'Пожаловаться'),
+                el('li', {class: 'reply'}, 'Ответить')
+            ])
+        ])
+    ]);
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('comment-wrap');
+    wrapper.appendChild(photoBlock);
+    wrapper.appendChild(commentBlock);
+    return wrapper;
+    /*return `<div class="comment-wrap">
     <div class="photo" title="${comment.author.name}">
       <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
     </div>
@@ -23,9 +59,9 @@ function createComment(comment) {
         </ul>
       </div>
     </div>
-  </div>`
+  </div>`*/
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
-  .then(res => res.json())
-  .then(showComments);
+    .then(res => res.json())
+    .then(showComments);
