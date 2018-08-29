e15e84cc2ca6e665e36a477614c286b054522df9
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-29 19:34:46 +0300

    done task 2 store

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function createElement(node) {
+    if (!node){
+        return document.createTextNode('');
+    }
+
+    if (Array.isArray(node)) {
+        return node.reduce(function(f, item) {
+            f.appendChild(createElement(item));
+
+            return f;
+        }, document.createDocumentFragment());
+    }
+
+    if(typeof node === 'string' || typeof node === 'number'){
+        return document.createTextNode(node.toString());
+    }
+
+    const element = document.createElement(node.name);
+
+
+    if (node.props) {
+        Object.keys(node.props).forEach(key => {
+            element.setAttribute(key, node.props[key]);
+        });
+    }
+
+    if (node.childs) {
+        element.appendChild(createElement(node.childs));
+    }
+
+    return element;
+
+}
