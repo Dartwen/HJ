73a5a3e9c75bfecabb7ee4828bce70e595c41eec
Dmitriy Petrov <dartwen@yandex.ru>
2018-08-31 14:35:34 +0300

    fixed task 2 todo-list

--- html-document-structure/todo-list/js/todo.js
+++ html-document-structure/todo-list/js/todo.js
@@ -1,14 +1,13 @@
 'use strict';
 
 function taskList(container) {
-    const done = Array.from(container.children).find(el => el.classList.contains('done'));
-    const undone = Array.from(container.children).find(el => el.classList.contains('undone'));
+    const done = document.querySelector('.done');
+    const undone = document.querySelector('.undone');
     const tasks = container.getElementsByTagName('label');
     Array.from(tasks).forEach(task => task.addEventListener('click', check));
 
     function check(event) {
         const input = event.currentTarget.firstElementChild;
-        input.checked = input.checked ? false : true;
         input.checked ? done.appendChild(event.currentTarget) : undone.appendChild(event.currentTarget);
     }
 }
